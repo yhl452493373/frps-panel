15438835b106d509a8f7730967d063926b913636
杨黄林 <yhl452493373@gmail.com>
2023-09-11 21:19:04 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -5,9 +5,9 @@ list=$(ls frps-panel-*)
 echo "$list"
 for binFile in $list
   do
-    tmpFile = frps-panel
+    tmpFile=frps-panel
     if echo "$binFile" | grep  -q -E "\.exe";then
-      tmpFile = frps-panel.exe
+      tmpFile=frps-panel.exe
     fi
     cp "$binFile" "$tmpFile"
     zip -r "$binFile".zip "$tmpFile" frps-panel.ini assets -x "*.git*" "*.idea*" "*.DS_Store" "*.contentFlavour"
