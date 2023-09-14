f41672a61365db8d03ddbe11c6d41720a0825f51
yanghuanglin <yanghuanglin@qq.com>
2023-09-14 13:05:54 +0800

    update package script

--- package.sh
+++ package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+version=$(cat cmd/frps-panel/cmd.go | grep 'const version' | egrep -o '[0-9.]+')
 cd ./release || exit
 rm -rf *.zip
 list=$(ls frps-panel-*)
@@ -6,11 +7,13 @@ echo "$list"
 for binFile in $list
   do
     tmpFile=frps-panel
+    newBinFile=$binFile
     if echo "$binFile" | grep  -q -E "\.exe";then
       tmpFile=frps-panel.exe
+      newBinFile=${newBinFile%%.exe*}
     fi
     cp "$binFile" "$tmpFile"
-    zip -r "$binFile".zip "$tmpFile" frps-panel.ini assets -x "*.git*" "*.idea*" "*.DS_Store" "*.contentFlavour"
+    zip -r "$newBinFile-$version".zip "$tmpFile" frps-panel.ini assets -x "*.git*" "*.idea*" "*.DS_Store" "*.contentFlavour"
     rm -rf "$binFile" "$tmpFile"
   done
   rm -rf frps-panel.ini assets
