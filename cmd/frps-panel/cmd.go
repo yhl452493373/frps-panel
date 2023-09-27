f6d2362f48895adaefd46bce746aa79f63fc1a81
yanghuanglin <yanghuanglin@qq.com>
2023-09-27 12:01:29 +0800

    fix dashboard_addr https issue

--- cmd/frps-panel/cmd.go
+++ cmd/frps-panel/cmd.go
@@ -87,7 +87,7 @@ func parseConfigFile(configFile, tokensFile string) (controller.HandleController
 		}
 	}
 
-	common.Common.DashboardTls = strings.HasPrefix("https://", strings.ToLower(common.Common.DashboardAddr))
+	common.Common.DashboardTls = strings.HasPrefix(strings.ToLower(common.Common.DashboardAddr), "https://")
 
 	tls := server.TLS{
 		Enable:   common.Common.TlsMode,
